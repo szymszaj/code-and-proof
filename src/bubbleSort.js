0c0fec68a625bdabaf2e7aaeb78de36e67dc454e
Szymon Zych <szymonzych868@gmail.com>
2025-07-16 21:37:17 +0200

    Add bubbleSort function to sort an array using the bubble sort algorithm

---
+++ src/bubbleSort.js
@@ -0,0 +1,13 @@
+function bubbleSort(arr) {
+  const n = arr.length;
+  const result = [...arr];
+
+  for (let i = 0; i < n - 1; i++) {
+    for (let j = 0; j < n - i - 1; j++) {
+      if (result[j] > result[j + 1]) {
+        [result[j], result[j + 1]] = [result[j + 1], result[j]];
+      }
+    }
+  }
+  return result;
+}
