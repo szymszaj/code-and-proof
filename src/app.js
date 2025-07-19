0ea9a546cf001b593722b80305edd1bcaa4a5f45
Szymon Zych <szymonzych868@gmail.com>
2025-07-19 23:51:00 +0200

    Add string manipulation functions: reverseString, isPalindrome, and removeDuplicates

---
+++ src/app.js
@@ -0,0 +1,12 @@
+function reverseString(str) {
+  return str.split("").reverse().join("");
+}
+
+function isPalindrome(str) {
+  const cleaned = str.toLowerCase().replace(/[^a-zA-Z0-9]/g, "");
+  return cleaned === cleaned.split("").reverse().join("");
+}
+
+function removeDuplicates(arr) {
+  return [...new Set(arr)];
+}
