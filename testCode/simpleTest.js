307642d3bac4cff2e445d65d21151a5fdf5a9fbb
Szymon Zych <szymonzych868@gmail.com>
2025-07-23 22:46:00 +0200

    Add isPalindrome function with test cases

---
+++ testCode/simpleTest.js
@@ -0,0 +1,7 @@
+function isPalindrome(str) {
+  const cleaned = str.replace(/[^a-z0-9]/gi, "").toLowerCase();
+  return cleaned === cleaned.split("").reverse().join("");
+}
+
+console.log(isPalindrome("A man, a plan, a canal: Panama")); // true
+console.log(isPalindrome("race a car")); // false
